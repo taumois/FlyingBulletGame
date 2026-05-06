f272e04aebb35605a4fcea7484cd498f507b1e4e
taumois <taumois@whs.school.nz>
2026-05-06 12:20:23 +1200

    Update

--- flying-bullet/bullet_character_body.gd
+++ flying-bullet/bullet_character_body.gd
@@ -1,11 +1,12 @@
 extends RigidBody2D
 
+const SPEED = 360
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+
+
+func _process(delta: float) -> void:
+	var direction = self.position.direction_to(get_global_mouse_position())
+	apply_force(Vector2(direction * SPEED))
