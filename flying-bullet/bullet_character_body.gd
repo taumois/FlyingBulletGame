0eb5a711d1c4f1d864925e7f672eb9906a98e1f1
taumois <taumois@whs.school.nz>
2026-05-07 09:39:38 +1200

    Update

--- flying-bullet/bullet_character_body.gd
+++ flying-bullet/bullet_character_body.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-const SPEED = 360
+const SPEED = 0
 
 
 func _ready() -> void:
@@ -8,5 +8,14 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	var direction = self.position.direction_to(get_global_mouse_position())
-	apply_force(Vector2(direction * SPEED))
+	var direction = self.position.direction_to(get_local_mouse_position())
+	apply_force(Vector2(direction * SPEED * delta / 0.0333))
+	if self.rotation > self.position.direction_to(get_local_mouse_position()).angle():
+		apply_torque(500)
+		print(1453256)
+	else:
+		apply_torque(-500)
+		print(0)
+	rotate()
+	#print(self.rotation_degrees)
+	#print(rad_to_deg(direction.angle())-90)
