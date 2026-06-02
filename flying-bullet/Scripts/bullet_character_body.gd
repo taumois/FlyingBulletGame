403201854f5a5d75824cf013b82c5cf0a070a2f6
taumois <238574759+taumois@users.noreply.github.com>
2026-06-02 18:36:50 +1200

    Clear movement logic

--- flying-bullet/Scripts/bullet_character_body.gd
+++ flying-bullet/Scripts/bullet_character_body.gd
@@ -17,28 +17,7 @@ func _physics_process(delta: float) -> void:
 	pass
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	var vector_to_cursor = self.position.direction_to(get_global_mouse_position())
-	
-	var isColliding = (state.get_contact_count() > 0)
-	var burstCoolDownUp = ($BurstCooldown.time_left == 0)
-	if isColliding || burstCoolDownUp:
-		state.apply_impulse(Vector2(vector_to_cursor * BURST_ACCELERATION * 0.033))
-		$BurstCooldown.start()
-	state.apply_force(Vector2(vector_to_cursor * ACCELERATION))
-	
-	var x = self.rotation + deg_to_rad(-90)
-	var x2 = self.rotation + deg_to_rad(90)
-	var y = state.linear_velocity.angle()
-	var z = rad_to_deg(angle_difference(x, y))
-	var z2 = rad_to_deg(angle_difference(x2, y))
-	z = sqrt(abs(z)) * Vector2(0, 0).length() * sign(z)
-	z2 = sqrt(abs(z2)) * sign(z2)
-	var u = pow(z, 2) * sign(z)
-	if u < 90 && u > -90:
-		state.apply_force(Vector2(vector_to_cursor * ACCELERATION).bounce(Vector2.from_angle(self.rotation))/abs(z))
-	else:
-		state.apply_force(Vector2(vector_to_cursor * ACCELERATION).bounce(Vector2.from_angle(self.rotation))/abs(z))
-	print((state.linear_velocity.bounce(Vector2.from_angle(self.rotation))/abs(z)).length())
-	
+	pass
+
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
 	pass
