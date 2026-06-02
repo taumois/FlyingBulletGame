46e0b91a66822b39b65850e4a78843eebf1beac8
taumois <238574759+taumois@users.noreply.github.com>
2026-06-02 20:43:29 +1200

    Update

--- flying-bullet/Scripts/bullet_character_body.gd
+++ flying-bullet/Scripts/bullet_character_body.gd
@@ -1,8 +1,7 @@
 extends RigidBody2D
 
-const ACCELERATION = 1000
-const BURST_ACCELERATION = 0
-const TURNING_ACCELERATION = 5000
+const ACCELERATION = 5000
+const TURNING_ACCELERATION = 500000
 
 
 func _ready() -> void:
@@ -14,10 +13,17 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	pass
-
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	pass
+	apply_force(Vector2.from_angle(self.rotation) * ACCELERATION * delta)
+	
+	if Input.is_action_pressed("ui_left"):
+		apply_torque(-TURNING_ACCELERATION * delta)
+	if Input.is_action_pressed("ui_right"):
+		apply_torque(TURNING_ACCELERATION * delta)
+	
+	#if true:
+		#apply_force(rotation_as_vector.rotated(deg_to_rad(90)) * 10)
+	#else:
+		#apply_force(rotation_as_vector.rotated(deg_to_rad(-90)) * 10)
 
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
 	pass
