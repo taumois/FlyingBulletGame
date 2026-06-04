4b9ba032832d8f0274ef2707c95eff21f2c9b26a
taumois <238574759+taumois@users.noreply.github.com>
2026-06-04 22:06:02 +1200

    Update,

--- flying-bullet/scripts/bullet.gd
+++ flying-bullet/scripts/bullet.gd
@@ -2,15 +2,33 @@ extends CharacterBody2D
 
 
 const MAX_HEARTS = 3
-const ACCELERATION = 100
+const ACCELERATION = 1 / 0.017
+const drag = 1.05
 
 var hearts
 
 func _ready() -> void:
-	hearts = MAX_HEARTS
+	self.hearts = MAX_HEARTS
 
 func _physics_process(delta: float) -> void:
-	var motion = Vector2.ZERO
+	velocity += Vector2.from_angle(self.rotation) * ACCELERATION
 	
+	velocity = velocity / drag
 	
-	move_and_collide(motion)
+	var collision = move_and_collide(velocity * delta)
+	if collision != null:
+		bounce(collision)
+
+
+func bounce(collision: KinematicCollision2D) -> void:
+	$CollisionPolygon2D.scale = Vector2.ZERO
+	$BounceLength.start()
+	collision.get_collider_id()
+	self.position = collision.get_position()
+	var collisionAngleVector = Vector2.from_angle(collision.get_angle())
+	velocity = velocity.bounce(collisionAngleVector)
+	rotation = Vector2.from_angle(rotation).bounce(collisionAngleVector).angle()
+
+
+func _on_bounce_length_timeout() -> void:
+	$CollisionPolygon2D.scale = Vector2.ONE
