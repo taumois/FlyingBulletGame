0e1d55de407f48ca4831ec8461fdb85e99e2dff2
Isaiah <taumois@whs.school.nz>
2026-06-05 13:20:02 +1200

    Update,

--- flying-bullet/scripts/bullet.gd
+++ flying-bullet/scripts/bullet.gd
@@ -2,18 +2,29 @@ extends CharacterBody2D
 
 
 const MAX_HEARTS = 3
-const ACCELERATION = 1 / 0.017
+const ROTATIONAL_ACCELERATION = 0.05 / 0.017
+const LINEAR_ACCELERATION = 100 / 0.017
 const drag = 1.05
 
+
+var blacklistedCollisionId
 var hearts
+
 
 func _ready() -> void:
 	self.hearts = MAX_HEARTS
 
+
 func _physics_process(delta: float) -> void:
-	velocity += Vector2.from_angle(self.rotation) * ACCELERATION
-	
+	velocity += Vector2.from_angle(self.rotation) * LINEAR_ACCELERATION * delta
 	velocity = velocity / drag
+	
+	var wants_to_turn_left = Input.is_action_pressed("turn_left")
+	var wants_to_turn_right = Input.is_action_pressed("turn_right")
+	var rotation_direction = (-1 if wants_to_turn_left else 0 + 1 if wants_to_turn_right else 0)
+	if rotation_direction != 0:
+		rotate(rotation_direction * ROTATIONAL_ACCELERATION * delta)
+		velocity = velocity / drag
 	
 	var collision = move_and_collide(velocity * delta)
 	if collision != null:
@@ -21,14 +32,12 @@ func _physics_process(delta: float) -> void:
 
 
 func bounce(collision: KinematicCollision2D) -> void:
-	$CollisionPolygon2D.scale = Vector2.ZERO
-	$BounceLength.start()
-	collision.get_collider_id()
+	var collisionId = collision.get_collider_id()
+	if collisionId == blacklistedCollisionId:
+		return
+	self.blacklistedCollisionId = collisionId
+	
 	self.position = collision.get_position()
-	var collisionAngleVector = Vector2.from_angle(collision.get_angle())
-	velocity = velocity.bounce(collisionAngleVector)
-	rotation = Vector2.from_angle(rotation).bounce(collisionAngleVector).angle()
-
-
-func _on_bounce_length_timeout() -> void:
-	$CollisionPolygon2D.scale = Vector2.ONE
+	var collisionNormal = collision.get_normal()
+	self.velocity = velocity.bounce(collisionNormal)
+	self.rotation = velocity.angle()
