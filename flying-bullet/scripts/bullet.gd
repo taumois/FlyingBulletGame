11f9ad36e50cc8c04bafa781738821a9667e806a
taumois <238574759+taumois@users.noreply.github.com>
2026-06-05 20:45:28 +1200

    Update bullet.gd

--- flying-bullet/scripts/bullet.gd
+++ flying-bullet/scripts/bullet.gd
@@ -1,8 +1,13 @@
 extends CharacterBody2D
 
+# Make rotation actually acceleration based
+# keep turning changing momentum direction
+# fine tune acceleration, drag, etc, values
+# switch from move_and_collide() to move_and_slide()
+# add speed number to HUD
 
 const MAX_HEARTS = 3
-const ROTATIONAL_ACCELERATION = 0.035 / 0.017
+const ROTATIONAL_ACCELERATION = 5 / 0.017
 const LINEAR_ACCELERATION = 0.65 / 0.017
 const linear_drag = 1.02
 const rotational_drag = 1.005
@@ -20,15 +25,15 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	velocity += Vector2.from_angle(self.rotation) * LINEAR_ACCELERATION * delta
+	velocity += Vector2.from_angle(rotation) * LINEAR_ACCELERATION * delta
 	velocity = velocity / linear_drag
 	
 	var wants_to_turn_left = Input.is_action_pressed("turn_left")
 	var wants_to_turn_right = Input.is_action_pressed("turn_right")
 	var rotation_direction = (-1 if wants_to_turn_left else 0 + 1 if wants_to_turn_right else 0)
 	if rotation_direction != 0:
-		rotate(rotation_direction * ROTATIONAL_ACCELERATION * delta)
-		velocity = velocity / rotational_drag
+		rotate(rotation_direction * ROTATIONAL_ACCELERATION / velocity.length_squared() * delta)
+		velocity = Vector2.from_angle(rotation) * (velocity.length() * 0.9)
 	
 	var collision = move_and_collide(velocity)
 	if collision != null:
@@ -46,9 +51,9 @@ func bounce(collision: KinematicCollision2D) -> void:
 	var collisionNormal = collision.get_normal()
 	self.velocity = velocity.bounce(collisionNormal)
 	self.rotation = self.velocity.angle()
-	self.position = collision.get_position() + self.velocity.normalized() * 33
+	self.position = collision.get_position() + self.velocity.normalized() * 1
 	
-	self.velocity *= 1.2
+	self.velocity *= 2
 	
 	
 	
