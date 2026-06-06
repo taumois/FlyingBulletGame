c028fd34debb2cf631372ee7f56dd2a4844c55b5
taumois <238574759+taumois@users.noreply.github.com>
2026-06-07 00:11:12 +1200

    Update

--- flying-bullet/scripts/bullet.gd
+++ flying-bullet/scripts/bullet.gd
@@ -1,42 +1,47 @@
 extends CharacterBody2D
 
-# Make rotation actually acceleration based
-# keep turning changing momentum direction
-# fine tune acceleration, drag, etc, values
-# add speed number to HUD
+signal score_updated(score: int)
+signal health_updated(health: int)
 
-const MAX_HEARTS = 3
+const MAX_HEALTH = 3
 const LINEAR_ACCELERATION = 0.01 / 0.01666666666667
 const LINEAR_SLIPPERINESS = 1 / 0.01666666666667
 
 var lastCollisionsCollider
-var hearts
+var health
 var score
 
 func _ready() -> void:
-	hearts = MAX_HEARTS
-	score = 0
+    health = MAX_HEALTH
+    score = 0
+    emit_signal("score_updated", score)
+    emit_signal("health_updated", health)
 
 func _process(_delta: float) -> void:
-	pass
+    pass
+    
 
 func _physics_process(delta: float) -> void:
-	velocity += Vector2.from_angle(rotation) * LINEAR_ACCELERATION * delta
-	#velocity *= LINEAR_SLIPPERINESS * delta
-	var collision = move_and_collide(velocity)
-	if collision != null:
-		bounce(collision)
+    velocity += Vector2.from_angle(rotation) * LINEAR_ACCELERATION * delta
+    #velocity *= LINEAR_SLIPPERINESS * delta
+    var collision = move_and_collide(velocity)
+    if collision != null:
+        bounce(collision)
 
 
 func bounce(collision: KinematicCollision2D) -> void:
-	var collisionNormal
-	
-	if lastCollisionsCollider != null:
-		lastCollisionsCollider.set_collision_layer_value(1, true)
-	lastCollisionsCollider = collision.get_collider()
-	lastCollisionsCollider.set_collision_layer_value(1, false)
-	
-	collisionNormal = collision.get_normal()
-	velocity = velocity.bounce(collisionNormal)
-	rotation = Vector2.from_angle(rotation).bounce(collisionNormal).angle()
-	position = collision.get_position()
+    var collisionNormal
+    
+    if lastCollisionsCollider != null:
+        lastCollisionsCollider.set_collision_layer_value(1, true)
+    lastCollisionsCollider = collision.get_collider()
+    lastCollisionsCollider.set_collision_layer_value(1, false)
+    
+    collisionNormal = collision.get_normal()
+    velocity = velocity.bounce(collisionNormal)
+    rotation = Vector2.from_angle(rotation).bounce(collisionNormal).angle()
+    position = collision.get_position()
+
+
+class x:
+    var _health
--- flying-bullet/scripts/hud.gd
+++ flying-bullet/scripts/hud.gd
@@ -1,17 +1,23 @@
 extends CanvasLayer
 
 const HEART = preload("res://scenes/heart.tscn")
+const  SCORE_TEXT = "Score: {0}"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	update_score(1)
 	for heart in %Bullet.MAX_HEARTS:
 		%Hearts.add_child(HEART.instantiate())
 
 
-func update_score() -> void:
-	%Score.text = "Score: "+%Bullet.score
+func update_score(score: int) -> void:
+	%Score.text = SCORE_TEXT.format([score])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_bullet_score_updated(score: int) -> void:
+	update_score(score)
