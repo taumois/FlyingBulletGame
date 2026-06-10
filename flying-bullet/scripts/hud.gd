d791a06bb7b82c0b83d14cb6adc3fe7fee752e3b
taumois <238574759+taumois@users.noreply.github.com>
2026-06-10 19:55:15 +1200

    Revert "Update"
    This reverts commit 24834a517db1290e681044cf48bc09512ec43622.

--- flying-bullet/scripts/hud.gd
+++ flying-bullet/scripts/hud.gd
@@ -1,11 +1,6 @@
 extends CanvasLayer
 
 const HEART = preload("res://scenes/heart.tscn")
-<<<<<<< Updated upstream
-=======
-
-var recorded_health = 0
->>>>>>> Stashed changes
 
 var recorded_health;
 var time
@@ -13,21 +8,14 @@ var acceleration
 
 
 func _ready() -> void:
-<<<<<<< Updated upstream
 	recorded_health = 0
 	acceleration = 0
 
 
-=======
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
->>>>>>> Stashed changes
 func _process(_delta: float) -> void:
 	pass
 
 
-<<<<<<< Updated upstream
 func _on_bullet_current_health(health: int) -> void:
 	for health_point in (health - recorded_health):
 		%Hearts.add_child(HEART.instantiate())
@@ -43,16 +31,3 @@ func _on_bullet_current_score(score: int) -> void:
 func _on_bullet_current_speed(speed: int) -> void:
 	time = time - Time.get_ticks_msec()
 	%Speed.text = "Speed {0}".format([acceleration])
-=======
-func _on_bullet_score_updated(score: int) -> void:
-	%Score.text = "Score: {0}".format([score])
-
-
-func _on_bullet_health_updated(health: int) -> void:
-	for i in health - recorded_health:
-		%Hearts.add_child(HEART.instantiate())
-	for i in recorded_health - health:
-		at
-		%Hearts.get_child(0).queue_free()
-	recorded_health = health
->>>>>>> Stashed changes
