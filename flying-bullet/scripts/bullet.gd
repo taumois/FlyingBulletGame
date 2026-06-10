24834a517db1290e681044cf48bc09512ec43622
taumois <238574759+taumois@users.noreply.github.com>
2026-06-10 19:54:32 +1200

    Update

--- flying-bullet/scripts/bullet.gd
+++ flying-bullet/scripts/bullet.gd
@@ -5,7 +5,7 @@ signal current_health(health: int)
 signal current_speed(speed: int)
 
 const MAX_HEALTH = 3
-const LINEAR_ACCELERATION = 0.01 / 0.01666666666667
+const LINEAR_ACCELERATION = 0.03 / 0.01666666666667
 const LINEAR_SLIPPERINESS = 1 / 0.01666666666667
 
 var lastCollisionsCollider
@@ -15,10 +15,19 @@ var score
 func _ready() -> void:
 	health = MAX_HEALTH
 	score = 0
+<<<<<<< Updated upstream
 
 func _process(_delta: float) -> void:
 	emit_signal("current_score", score)
 	emit_signal("current_health", health)
+=======
+	emit_signal("score_updated", score)
+	emit_signal("health_updated", health)
+
+func _process(_delta: float) -> void:
+	pass
+	
+>>>>>>> Stashed changes
 
 func _physics_process(delta: float) -> void:
 	velocity += Vector2.from_angle(rotation) * LINEAR_ACCELERATION * delta
@@ -26,8 +35,11 @@ func _physics_process(delta: float) -> void:
 	var collision = move_and_collide(velocity)
 	if collision != null:
 		bounce(collision)
+<<<<<<< Updated upstream
 	
 	emit_signal("current_speed", velocity.length())
+=======
+>>>>>>> Stashed changes
 
 
 func bounce(collision: KinematicCollision2D) -> void:
@@ -42,6 +54,11 @@ func bounce(collision: KinematicCollision2D) -> void:
 	velocity = velocity.bounce(collisionNormal)
 	rotation = Vector2.from_angle(rotation).bounce(collisionNormal).angle()
 	position = collision.get_position()
+<<<<<<< Updated upstream
+=======
+	health -= 1
+	emit_signal("health_updated", health)
+>>>>>>> Stashed changes
 
 
 class x:
--- flying-bullet/scripts/hud.gd
+++ flying-bullet/scripts/hud.gd
@@ -1,6 +1,11 @@
 extends CanvasLayer
 
 const HEART = preload("res://scenes/heart.tscn")
+<<<<<<< Updated upstream
+=======
+
+var recorded_health = 0
+>>>>>>> Stashed changes
 
 var recorded_health;
 var time
@@ -8,14 +13,21 @@ var acceleration
 
 
 func _ready() -> void:
+<<<<<<< Updated upstream
 	recorded_health = 0
 	acceleration = 0
 
 
+=======
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+>>>>>>> Stashed changes
 func _process(_delta: float) -> void:
 	pass
 
 
+<<<<<<< Updated upstream
 func _on_bullet_current_health(health: int) -> void:
 	for health_point in (health - recorded_health):
 		%Hearts.add_child(HEART.instantiate())
@@ -31,3 +43,16 @@ func _on_bullet_current_score(score: int) -> void:
 func _on_bullet_current_speed(speed: int) -> void:
 	time = time - Time.get_ticks_msec()
 	%Speed.text = "Speed {0}".format([acceleration])
+=======
+func _on_bullet_score_updated(score: int) -> void:
+	%Score.text = "Score: {0}".format([score])
+
+
+func _on_bullet_health_updated(health: int) -> void:
+	for i in health - recorded_health:
+		%Hearts.add_child(HEART.instantiate())
+	for i in recorded_health - health:
+		at
+		%Hearts.get_child(0).queue_free()
+	recorded_health = health
+>>>>>>> Stashed changes
