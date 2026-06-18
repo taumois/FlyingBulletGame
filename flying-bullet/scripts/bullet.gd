33738d8f6c7ab7bac8f55eb71cffc575f599fa5e
taumois <238574759+taumois@users.noreply.github.com>
2026-06-18 22:32:03 +1200

    Update

--- flying-bullet/scripts/bullet.gd
+++ flying-bullet/scripts/bullet.gd
@@ -5,9 +5,9 @@ signal current_health(health: int)
 signal current_speed(speed: int)
 
 const MAX_HEALTH = 3
-const LINEAR_ACCELERATION = 1.0
+const LINEAR_ACCELERATION = 0.0575
 const ROTATIONAL_ACCELERATION = 0.0
-const LINEAR_DRAG_COEFFICIENT = 1.0
+const LINEAR_DRAG_COEFFICIENT = 0.0000001
 const ROTATIONAL_DRAG_COEFFICIENT = 0.0
 const SCORE_GAIN_ON_BOUNCE = 5
 
@@ -15,6 +15,7 @@ var previous_collisions_collider
 var health
 var score
 var turn_direction
+var linear_velocity
 var rotational_velocity
 
 enum Direction {
@@ -25,9 +26,9 @@ enum Direction {
 
 func _ready() -> void:
 	previous_collisions_collider = StaticBody2D.new()
-	velocity = Vector2.ZERO
 	rotation = 0.0
 	turn_direction = Direction.NEUTRAL
+	linear_velocity = Vector2i.ZERO
 	rotational_velocity = 0.0
 	health = MAX_HEALTH
 	score = 0
@@ -51,40 +52,48 @@ func _process(_delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
+	var speed = linear_velocity.length()
+	emit_signal("current_speed", speed)
+	
 	if turn_direction != null:
 		rotational_velocity += turn_direction * ROTATIONAL_ACCELERATION
 	
-	var rotational_drag = 1
+	var rotational_drag = _drag_calculated(rotational_velocity + speed, LINEAR_DRAG_COEFFICIENT)
 	rotational_velocity /= rotational_drag
 	
 	rotation += rotational_velocity
 	
-	velocity = Vector2.from_angle(rotation) * velocity.length() + Vector2.from_angle(rotation) * LINEAR_ACCELERATION
+	linear_velocity = Vector2.from_angle(rotation) * speed + Vector2.from_angle(rotation) * LINEAR_ACCELERATION
 	
-	var linear_drag = pow(velocity.length() + rotational_velocity, 2.0) * LINEAR_DRAG_COEFFICIENT
-	velocity /= linear_drag
+	var linear_drag = _drag_calculated(speed + rotational_velocity, LINEAR_DRAG_COEFFICIENT)
+	linear_velocity /= linear_drag
 	
-	var collision = move_and_collide(velocity)
+	
+	var collision = move_and_collide(linear_velocity)
 	if collision != null:
 		bounce(collision)
 	elif (previous_collisions_collider.get_collision_layer_value(1) == false) and (not in_area_of_previous_collisions_collider()):
 		previous_collisions_collider.set_collision_layer_value(1, true)
-		
-	emit_signal("current_speed", velocity.length())
+
+
+func _drag_calculated(_velocity: float, drag_coefficient: float) -> float:
+	var drag = 1 + _velocity ** 2.0 * drag_coefficient
+	return drag
+
 
 
 func bounce(collision: KinematicCollision2D) -> void:
 	var collision_normal = collision.get_normal()
 	var collisions_collider = collision.get_collider()
 	
-	velocity = velocity.bounce(collision_normal)
+	linear_velocity = linear_velocity.bounce(collision_normal)
 	rotation = Vector2.from_angle(rotation).bounce(collision_normal).angle()
 	position = collision.get_position()
 	
 	score += SCORE_GAIN_ON_BOUNCE
 	
 	if collisions_collider.has_method("material_bounciness"):
-		velocity *= collisions_collider.acceleration_multiplication_from_collision()
+		linear_velocity *= collisions_collider.acceleration_multiplication_from_collision()
 	
 	previous_collisions_collider.set_collision_layer_value(1, true)
 	collisions_collider.set_collision_layer_value(1, false)
--- flying-bullet/scripts/camera.gd
+++ flying-bullet/scripts/camera.gd
@@ -1,11 +1,19 @@
 extends Camera2D
 
+var zoom_velocity
+var target_zoom
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	zoom_velocity = 0
 
+func _physics_process(delta: float) -> void:
+	if zoom.x < target_zoom:
+		zoom_velocity += 0.2
+	else:
+		zoom_velocity -= 0.2
+	zoom_velocity * 0.99
+	zoom.x += zoom_velocity
+	zoom.y += zoom_velocity
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _on_bullet_current_speed(speed: int) -> void:
+	target_zoom = 1.0 + sqrt(speed)
