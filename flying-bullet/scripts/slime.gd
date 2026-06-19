f4e9b003e3234e1755ab2fb5be7371332f728c50
Isaiah <taumois@whs.school.nz>
2026-06-19 13:20:24 +1200

    Update slime.gd

--- flying-bullet/scripts/slime.gd
+++ flying-bullet/scripts/slime.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-const LINEAR_ACCELERATION = 100;
+const LINEAR_ACCELERATION = 1000.0;
 const MAX_HEALTH = 100
 
 var health
@@ -8,13 +8,18 @@ var bullet
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	linear_damp = 1.0
+	angular_damp = 1.0
 	bullet = get_tree().root.get_node("World/Bullet")
 	health = MAX_HEALTH
-	
 
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+func _physics_process(delta: float) -> void:
 	var angle_to_bullet = get_angle_to(bullet.position)
-	state.apply_force(Vector2.from_angle(angle_to_bullet) * LINEAR_ACCELERATION)
+	apply_force(Vector2.from_angle(angle_to_bullet) * LINEAR_ACCELERATION)
+
+#func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	#var angle_to_bullet = get_angle_to(bullet.position)
+	#state.apply_force(Vector2.from_angle(angle_to_bullet) * LINEAR_ACCELERATION)
 
 #func damage_from_collision() -> int:
 	#return 
