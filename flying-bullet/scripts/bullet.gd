e73b2df28b4b35cb2de69fa83b3969173d1b882f
taumois <238574759+taumois@users.noreply.github.com>
2026-06-20 19:09:15 +1200

    Update

--- flying-bullet/scripts/bullet.gd
+++ flying-bullet/scripts/bullet.gd
@@ -4,14 +4,14 @@ signal current_score(score: int)
 signal current_health(health: int)
 signal current_speed(speed: float)
 
-const INITIAL_LINEAR_VELOCITY = Vector2.RIGHT * 10.0
+const INITIAL_LINEAR_VELOCITY = Vector2.RIGHT * 3.33
 const MAX_HEALTH = 3
 const LINEAR_ACCELERATION = 0.0
-const ROTATIONAL_ACCELERATION = 0.1
-const LINEAR_DRAG_LINEAR_FACTOR_COEFFICIENT = 0.002
-const LINEAR_DRAG_ROTATIONAL_FACTOR_COEFFICIENT = 1500.0
-const ROTATIONAL_DRAG_LINEAR_FACTOR_COEFFICIENT = 100.0
-const ROTATIONAL_DRAG_ROTATIONAL_FACTOR_COEFFICIENT = 100.0
+const ROTATIONAL_ACCELERATION = 0.02
+const LINEAR_DRAG_LINEAR_FACTOR_COEFFICIENT = 0.0015
+const LINEAR_DRAG_ROTATIONAL_FACTOR_COEFFICIENT = 0.0
+const ROTATIONAL_DRAG_LINEAR_FACTOR_COEFFICIENT = 0.00225
+const ROTATIONAL_DRAG_ROTATIONAL_FACTOR_COEFFICIENT = 6.7
 const SCORE_GAIN_ON_BOUNCE = 5
 const FPS_DEVELOPED_IN = 60
 const BOUNCED_LINEAR_VELOCITY_COEFFICIENT = 2.0
@@ -44,13 +44,11 @@ func _ready() -> void:
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
-	if event.is_action_pressed("turn_left"):
-		turn_direction = Direction.LEFT
-	elif event.is_action_released("turn_left"):
-		turn_direction = Direction.NEUTRAL
-	if event.is_action_pressed("turn_right"):
+	if Input.is_action_pressed("turn_right"):
 		turn_direction = Direction.RIGHT
-	elif event.is_action_released("turn_right"):
+	elif Input.is_action_pressed("turn_left"):
+		turn_direction = Direction.LEFT
+	else:
 		turn_direction = Direction.NEUTRAL
 	get_viewport().set_input_as_handled()
 
@@ -62,16 +60,19 @@ func _process(_delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	var special_delta = delta * FPS_DEVELOPED_IN
-	print(delta)
 	
 	var speed = linear_velocity.length()
 	emit_signal("current_speed", speed)
 	
 	if turn_direction != null:
 		rotational_velocity += turn_direction * ROTATIONAL_ACCELERATION * special_delta
-	
+	var rotational_drag = _drag_calculated(rotational_velocity * ROTATIONAL_DRAG_ROTATIONAL_FACTOR_COEFFICIENT + (speed if rotational_velocity >= 0 else -speed) * ROTATIONAL_DRAG_LINEAR_FACTOR_COEFFICIENT)
+	rotational_velocity /= rotational_drag * special_delta
 	rotation += rotational_velocity * special_delta
+	
 	linear_velocity = Vector2.from_angle(rotation) * speed + Vector2.from_angle(rotation) * LINEAR_ACCELERATION * special_delta
+	var linear_drag = _drag_calculated(speed * LINEAR_DRAG_LINEAR_FACTOR_COEFFICIENT + rotational_velocity * LINEAR_DRAG_ROTATIONAL_FACTOR_COEFFICIENT)
+	linear_velocity /= linear_drag * special_delta
 	
 	if (previous_collisions_collider.get_collision_layer_value(1) == false) and (not in_area_of_previous_collisions_collider()):
 		previous_collisions_collider.set_collision_layer_value(1, true)
@@ -82,16 +83,10 @@ func _physics_process(delta: float) -> void:
 			pass
 	elif (previous_collisions_collider.get_collision_layer_value(1) == false) and (not in_area_of_previous_collisions_collider()):
 		previous_collisions_collider.set_collision_layer_value(1, true)
-	
-	var rotational_drag = _drag_calculated(rotational_velocity * ROTATIONAL_DRAG_ROTATIONAL_FACTOR_COEFFICIENT + speed * ROTATIONAL_DRAG_LINEAR_FACTOR_COEFFICIENT)
-	rotational_velocity /= rotational_drag * special_delta
-	
-	var linear_drag = _drag_calculated(speed * LINEAR_DRAG_LINEAR_FACTOR_COEFFICIENT + rotational_velocity * LINEAR_DRAG_ROTATIONAL_FACTOR_COEFFICIENT)
-	linear_velocity /= linear_drag * special_delta
 
 
 func _drag_calculated(_velocity: float) -> float:
-	var drag = 1 + _velocity ** DRAG_EXPONENT
+	var drag = 1 + _velocity * _velocity
 	return drag
 
 
@@ -106,7 +101,6 @@ func attempt_bounce(collision: KinematicCollision2D) -> bool:
 	rotation = Vector2.from_angle(rotation).bounce(collision_normal).angle()
 	position = collision.get_position()
 	if in_area_of_previous_collisions_collider():
-		print(1)
 		position = saved_position
 		rotation = saved_rotation
 		return false
--- flying-bullet/scripts/camera_target_alterer.gd
+++ flying-bullet/scripts/camera_target_alterer.gd
@@ -3,14 +3,20 @@ extends Node2D
 signal current_scale(scale: float)
 
 const SCALE_COEFFICIENT = 1.0
-const VIEWPORT_FRACTION_OFFSET = 0.134
+const VIEWPORT_FRACTION_OFFSET = 0.25
 
+var offset_size
 var parent
-var viewport_width
 
 func _ready() -> void:
+	get_tree().root.size_changed.connect(viewport_rect_updated)
 	parent = get_parent()
-	viewport_width  = get_viewport_rect().size.x
+	viewport_rect_updated()
+
+
+func viewport_rect_updated():
+	var viewport_width = get_viewport_rect().size.x
+	offset_size = viewport_width * VIEWPORT_FRACTION_OFFSET
 
 
 func _on_bullet_current_speed(speed: float) -> void:
@@ -18,7 +24,6 @@ func _on_bullet_current_speed(speed: float) -> void:
 	emit_signal("current_scale", scale)
 	
 	var parent_rotation_vector = Vector2.from_angle(parent.rotation)
-	var offset_size = viewport_width * VIEWPORT_FRACTION_OFFSET
 	var offset_vector = parent_rotation_vector / scale * offset_size
 	
 	global_position = parent.global_position + offset_vector
