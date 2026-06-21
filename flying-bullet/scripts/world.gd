657a0cd0261d42d33bf6479e11989baa2ddfe6e5
taumois <238574759+taumois@users.noreply.github.com>
2026-06-21 13:24:42 +1200

    Update

--- flying-bullet/scripts/world.gd
+++ flying-bullet/scripts/world.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-const CHUNK_SIZE = 200
-const HOUSES_PER_CHUNK = 1
+const CHUNK_SIZE = 4000
+const HOUSES_PER_CHUNK = 6
 const HOUSE = preload("res://scenes/house.tscn")
 
 var bullet
@@ -27,19 +27,37 @@ func _process(delta: float) -> void:
 	bullet_chunk = new_bullet_chunk
 	
 	for i in loaded_chunks.size():
-				if not loaded_chunks[i].is_adjacent_to_chunk(bullet_chunk):
-					print(randf())
-					var new_chunk = Chunk.new(Vector2i((i % 3) - 1 + bullet_chunk.x, roundi(i / 3 - 0.25) - 0.5 + bullet_chunk.y))
-					loaded_chunks[i].remove_houses()
-					load_chunk(new_chunk)
-					loaded_chunks[i] = new_chunk
+		if not loaded_chunks[i].is_adjacent_to_chunk(bullet_chunk):
+			for j in loaded_chunks.size():
+					var new_chunk = Chunk.new(Vector2i((j % 3) - 1 + bullet_chunk.x, roundi(j / 3 - 0.25) - 0.5 + bullet_chunk.y))
+					if not chunk_in_loaded_chunks(new_chunk):
+						loaded_chunks[i].remove_houses()
+						load_chunk(new_chunk)
+						loaded_chunks[i] = new_chunk
 
 func load_chunk(chunk: Chunk) -> void:
-	var chunk_id = fmod(chunk.x * chunk.y * seed, 1.0)
-	var house = HOUSE.instantiate()
-	house.position = chunk.position() * CHUNK_SIZE + Vector2i(Vector2.ONE * chunk_id * CHUNK_SIZE)
-	chunk.houses.push_back(house)
-	add_child(house)
+	var chunk_unique_randf = fmod(chunk.x * chunk.y * seed, 1.0)
+	var house_unique_randf_x = fmod(rand_from_seed(chunk_unique_randf)[0] / chunk_unique_randf / PI, 1.0)
+	var house_unique_randf_y = fmod(rand_from_seed(chunk_unique_randf)[0] / chunk_unique_randf / seed, 1.0)
+	
+	for i in HOUSES_PER_CHUNK:
+		var house = HOUSE.instantiate()
+		house_unique_randf_x = fmod(rand_from_seed(house_unique_randf_x)[0] / house_unique_randf_y, 1.0)
+		house_unique_randf_y = fmod(rand_from_seed(house_unique_randf_y)[0] / house_unique_randf_x, 1.0)
+		house.position = chunk.position() * CHUNK_SIZE + Vector2i(house_unique_randf_x * CHUNK_SIZE, house_unique_randf_y * CHUNK_SIZE)
+		house.rotation = 2.0 * PI * chunk_unique_randf
+		house.scale.x = 5.0
+		house.scale.y = 5.0
+		chunk.houses.push_back(house)
+		add_child(house)
+
+
+func chunk_in_loaded_chunks(chunk: Chunk) -> bool:
+	for loaded_chunk in loaded_chunks:
+		if chunk.x == loaded_chunk.x:
+			if chunk.y == loaded_chunk.y:
+				return true
+	return false
 
 
 class Chunk:
