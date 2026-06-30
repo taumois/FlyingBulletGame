d812bd86c5c5ab24d1042807f73aac5f84ae5340
Isaiah <taumois@whs.school.nz>
2026-07-01 11:55:55 +1200

    Reapply "Update"
    This reverts commit 849da47321d496ef5fe08a80818976ce3234fe5f.

--- flying-bullet/scripts/death_explosion.gd
+++ flying-bullet/scripts/death_explosion.gd
@@ -1,8 +1,11 @@
 extends CPUParticles2D
 
 func _ready() -> void:
+	%ExplosionSound.volume_db = 250.0
+	%ExplosionSound.pitch_scale = randf_range(0.75, 1.25)
+	%ExplosionSound.play()
 	emitting = true
 
 
-func _on_finished() -> void:
+func _on_explosion_sound_finished() -> void:
 	queue_free()
